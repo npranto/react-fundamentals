d6aeaba6633082691e5d5cb511ffdadc73f9689f
Nazmuz Pranto <npranto@gmail.com>
2021-12-23 13:07:30 -0500

    completes exercise 5

---
+++ src/exercise/05.extra-1.js
@@ -0,0 +1,25 @@
+// Styling
+// http://localhost:3000/isolated/exercise/05.js
+
+import * as React from 'react'
+import '../box-styles.css'
+
+function Box({ children, className = '', style = {} }) {
+  return <div className={`box ${className}`} style={{ fontStyle: 'italic', ...style }}>{children}</div> 
+}
+
+const smallBox = <Box className="box--small" style={{backgroundColor: 'lightblue'}}> small lightblue box </Box>
+const mediumBox = <Box className="box--medium" style={{backgroundColor: 'pink'}}> medium pink box </Box>
+const largeBox = <Box className="box--large" style={{backgroundColor: 'orange'}}> large orange box </Box>
+
+function App() {
+  return (
+    <div>
+      {smallBox}
+      {mediumBox}
+      {largeBox}
+    </div>
+  )
+}
+
+export default App
---
+++ src/exercise/05.extra-2.js
@@ -0,0 +1,25 @@
+// Styling
+// http://localhost:3000/isolated/exercise/05.js
+
+import * as React from 'react'
+import '../box-styles.css'
+
+function Box({ children, className, size, style = {}, ...restProps }) {
+  return <div className={`box box--${size} ${className}`} style={{fontStyle: 'italic', ...style}} {...restProps}>{children}</div> 
+}
+
+const smallBox = <Box size="small" style={{backgroundColor: 'lightblue'}}> small lightblue box </Box>
+const mediumBox = <Box size="medium" style={{backgroundColor: 'pink'}}> medium pink box </Box>
+const largeBox = <Box size="large" style={{backgroundColor: 'orange'}}> large orange box </Box>
+
+function App() {
+  return (
+    <div>
+      {smallBox}
+      {mediumBox}
+      {largeBox}
+    </div>
+  )
+}
+
+export default App
